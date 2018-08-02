568b25d96c578ff57e7d74c559765a6e1b949449
Taylor Williams <taylor@thewilliams.net>
2018-08-02 22:06:29 +0000

    automatically backed up by learn

--- app/application.rb
+++ app/application.rb
@@ -6,11 +6,7 @@ class Application
     req = Rack::Request.new(env)
 
     if req.path =~ /items/
-      puts req.path.split("items/")[1]
-      puts @@items
       item = @@items.detect{|i|i.name == req.path.split("items/")[1]}
-      puts item.name
-      puts item.price
       if item
         resp.write "#{item.price}"
       else
