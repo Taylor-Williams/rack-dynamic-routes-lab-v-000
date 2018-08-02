4beb2a6d8a461ca7e9c2c786a4e8cdb35fd6e6bc
Taylor Williams <taylor@thewilliams.net>
2018-08-02 21:46:37 +0000

    automatically backed up by learn

---
+++ app/application.rb
@@ -0,0 +1,22 @@
+class Application
+
+  @@items = []
+  
+  def call(env)
+    resp = Rack::Response.new
+    req = Rack::Request.new(env)
+
+    if req.path =~ /items/
+      item = @@items.each.detect{|i|i.name = req.path.split("items")[1]}
+      if item 
+        return item.price
+      else
+        resp.write "i do not have that item"
+        req.status = 400
+    else
+      resp.write "page not found"
+      req.status = 404
+    end
+    resp.finish
+  end
+end
