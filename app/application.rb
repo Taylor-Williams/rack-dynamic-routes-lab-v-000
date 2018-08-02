81333dc2768e1840a4c7cb9fb6ed665090bb4702
Taylor Williams <taylor@thewilliams.net>
2018-08-02 22:03:57 +0000

    automatically backed up by learn

--- app/application.rb
+++ app/application.rb
@@ -7,6 +7,7 @@ class Application
 
     if req.path =~ /items/
       puts req.path.split("items/")[1]
+      puts items
       item = @@items.detect{|i|i.name = req.path.split("items/")[1]}
       puts item.name
       if item
